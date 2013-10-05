c9b0e00efed9dc987b33cb2f1b639f1d64210ebf
Naoto Yokoyama <builtinnya@gmail.com>
2013-10-05 12:52:55 +0900

    Fixed a bug that fails to click golden cookies
    Cookie Clicker changed its initialization mechanism to initialize Game
    after loading assets (images) asynchronously.
    This introduced the bug to the extension because Game.goldenCookie is
    undefined when registering the watchers.

--- src/js/page.js
+++ src/js/page.js
@@ -2,16 +2,29 @@
 
   var oldOnLoad = window.onload || function () {};
 
-  var initialize = function () {
-    pageMessaging.pageInitialize();
-    pageMessagingClient.sendToExtension({ cmd: 'ConfigClient' });
-  };
-
   window.onload = function () {
-    // Old `window.onload` initializes `Game` object.
+    if (!window.Game)
+      throw 'Error: Game is undefined.';
+
+    if (!window.Game.Init)
+      throw 'Error: Game.Init is undefined: This extension may not support this version of Cookie Clicker.';
+
+    var oldGameInit = window.Game.Init;
+
+    // Cookie Clicker calls Game.Init after loading assets asynchronously.
+    // So we replace Game.Init to make sure the client will be configured after
+    // initialization of Game.
+    window.Game.Init = function () {
+      // Call the initializer for Cookie Clicker.
+      oldGameInit();
+
+      // Then initialize the messaging module and the game client.
+      pageMessaging.pageInitialize();
+      pageMessagingClient.sendToExtension({ cmd: 'ConfigClient' });
+    };
+
+    // Don't forget to trigger the initialization.
     oldOnLoad();
-    // Now we can use `Game` object to play the game!
-    initialize();
   };
 
 })(window, pageMessaging, pageMessagingClient);
